027cafffee546d80612ceb07bf73b53888c7502c
2018514 <2018514@sluh.org>
2015-09-03 11:50:10 -0500

    Push

---
+++ src/Classroom.java
@@ -0,0 +1,8 @@
+/**
+ * Created by 2018514 on 9/3/15.
+ */
+public class Classroom {
+    public static void main(String [] args) {
+
+    }
+}
---
+++ src/Student.java
@@ -0,0 +1,45 @@
+/**
+ * Created by 2018514 on 9/3/15.
+ */
+public class Student {
+    private double sc1;
+    private double sc2;
+    private double sc3;
+
+    public Student(double sc1, double sc2, double sc3) {
+
+    }
+
+    public void setScore2(double score) {
+
+    }
+
+    public void setScore3(double score) {
+
+    }
+
+    public double getScore1() {
+
+        return 0;
+    }
+
+    public void setScore1(double score) {
+
+    }
+
+    public double setScore2() {
+
+        return 0;
+    }
+
+    public double setScore3() {
+
+        return 0;
+    }
+
+    public double getAverage() {
+
+        return 0;
+    }
+
+}
