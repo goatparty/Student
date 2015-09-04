09dd8ff3fab9f7821f1c387ac8e746ace99e0069
2018514 <2018514@sluh.org>
2015-09-04 11:49:49 -0500

    grgsd

--- src/Classroom.java
+++ src/Classroom.java
@@ -1,8 +1,21 @@
 /**
  * Created by 2018514 on 9/3/15.
  */
+import java.util.*;
 public class Classroom {
     public static void main(String [] args) {
+        Student student = new Student();
+        Scanner keyboard = new Scanner(System.in);
+        double sc1,sc2,sc3;
+
+        System.out.println("Enter score 1: ");
+        sc1 = keyboard.nextDouble();
+        System.out.println("Enter score 2: ");
+        sc2 = keyboard.nextDouble();
+        System.out.println("Enter score 3: ");
+        sc3 = keyboard.nextDouble();
+
+
 
     }
 }
--- src/Student.java
+++ src/Student.java
@@ -6,40 +6,87 @@ public class Student {
     private double sc2;
     private double sc3;
 
-    public Student(double sc1, double sc2, double sc3) {
-
+    /**
+     * Default constructor sets all the values to 0
+     * @param sc1
+     * @param sc2
+     * @param sc3
+     */
+    public Student() {
+        sc1 = 0;
+        sc2 = 0;
+        sc3 = 0;
+    }
+    /**
+     * Default constructor sets all the values to 0
+     * @param sc1
+     * @param sc2
+     * @param sc3
+     */
+    public Student(double score1, double score2, double score3) {
+        sc1 = score1;
+        sc2 = score2;
+        sc3 = score3;
     }
 
+    /**
+     * Sets value of score 1
+     * @param score
+     */
+    public void setScore1(double score) {
+        sc1 = score;
+    }
+
+    /**
+     * Sets value of score 2
+     * @param score
+     */
     public void setScore2(double score) {
-
+        sc2 = score;
     }
 
+    /**
+     * Sets value of score 3
+     * @param score
+     */
     public void setScore3(double score) {
-
+        sc3 = score;
     }
 
+    /**
+     * Returns value of score 1
+     * @return
+     */
     public double getScore1() {
 
-        return 0;
+        return sc1;
     }
 
-    public void setScore1(double score) {
-
-    }
-
+    /**
+     * Returns the value of score 2
+     * @return
+     */
     public double setScore2() {
 
-        return 0;
+        return sc2;
     }
 
+    /**
+     * returns value of score 3
+     * @return
+     */
     public double setScore3() {
 
-        return 0;
+        return sc3;
     }
 
+    /**
+     * Returns the average of the 3 scores combined
+     * @return
+     */
     public double getAverage() {
 
-        return 0;
+        return (float)(sc1 + sc2 + sc3)/3;
     }
 
 }
